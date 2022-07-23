64b1b11492915a83dbfa2841d5b7e51f877e05cd
Phillipe Smith <phsmithcc@gmail.com>
2022-07-23 15:11:13 -0300

    feat: test code coverate

--- Sonar/__tests__/calc_test.js
+++ Sonar/__tests__/calc_test.js
@@ -1,13 +1,13 @@
-const calc = require('../src/calc.js');
+const {sum, sub, multi, div} = require('../src/calc.js');
 
 test("sum 10 + 10 to be equal 20", () => {
-    expect(calc.sum(10, 10)).toBe(20);
+    expect(sum(10, 10)).toBe(20);
 });
 
 test("sub 10 - 10 to be equal 0", () => {
-    expect(calc.sub(10, 10)).toBe(0);
+    expect(sub(10, 10)).toBe(0);
 });
 
 test("multi 10 * 10 to be equal 100", () => {
-    expect(calc.multi(10, 10)).toBe(100);
+    expect(multi(10, 10)).toBe(100);
 });
